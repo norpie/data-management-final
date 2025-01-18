fe4b2acf00380f82e023a286e5a60addacb09c38
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-18 19:40:23 +0100

    feat: add sample users

---
+++ add_users.sql
@@ -0,0 +1,7 @@
+CALL add_user('user1', 'password');
+CALL add_user('user2', 'password');
+CALL add_user('user3', 'password');
+CALL add_user('user4', 'password');
+CALL add_user('user5', 'password');
+CALL add_user('user6', 'password');
+CALL add_user('user7', 'password');
