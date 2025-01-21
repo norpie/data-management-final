95f589e308237c1cb967788981b5570fd56a311e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-21 21:29:06 +0100

    feat: add random users

--- init/2_add_users.sql
+++ init/2_add_users.sql
@@ -1,7 +1,39 @@
-CALL add_user('user1', 'password');
-CALL add_user('user2', 'password');
-CALL add_user('user3', 'password');
-CALL add_user('user4', 'password');
-CALL add_user('user5', 'password');
-CALL add_user('user6', 'password');
-CALL add_user('user7', 'password');
+DELIMITER $$
+
+CREATE PROCEDURE generate_random_users(IN num_users INT)
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE random_username VARCHAR(255);
+    DECLARE random_password VARCHAR(255);
+    DECLARE name_list JSON;
+    DECLARE adjective_list JSON;
+
+    -- List of random names and adjectives
+    SET name_list = JSON_ARRAY('Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliet',
+                               'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango');
+    SET adjective_list = JSON_ARRAY('Fast', 'Brave', 'Smart', 'Loyal', 'Happy', 'Strong', 'Clever', 'Calm', 'Kind', 'Bold');
+
+    WHILE i <= num_users DO
+        -- Generate a random username from a combination of adjectives and names
+        SET random_username = CONCAT(
+            JSON_UNQUOTE(JSON_EXTRACT(adjective_list, CONCAT('$[', FLOOR(RAND() * 10), ']'))),
+            JSON_UNQUOTE(JSON_EXTRACT(name_list, CONCAT('$[', FLOOR(RAND() * 20), ']'))),
+            FLOOR(RAND() * 1000)
+        );
+
+        -- Generate a random password (8-12 characters long, alphanumeric)
+        SET random_password = CONCAT(
+            LEFT(MD5(RAND()), FLOOR(RAND() * 5) + 8)
+        );
+
+        -- Call the add_user procedure
+        CALL add_user(random_username, random_password);
+
+        SET i = i + 1;
+    END WHILE;
+END$$
+
+DELIMITER ;
+
+-- Example: Generate 50 random users
+CALL generate_random_users(100);
